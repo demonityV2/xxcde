6f3951ab744f22a2e9cf0f7111b0b4bc544d2157
demonityV2 <ytfazezachary7@gmail.com>
2025-03-04 11:31:38 -0500

    Update JumpShoot.lua

--- JumpShoot.lua
+++ JumpShoot.lua
@@ -1,3 +1,17 @@
+local modules = {
+    ["Character"] = {},
+}
+--
+for _, v in pairs(getgc(true)) do
+    if typeof(v) == "function" and islclosure(v) then
+        local info = debug.getinfo(v)
+        local name = string.match(info.short_src, "%.([%w_]+)$")
+
+        if name and modules[name] and info.name ~= nil then
+            modules[name][info.name] = info.func
+        end
+    end
+end
 local novas = {jumpshoot = false}
 local JS = debug.getupvalues(modules.Character.updateCharacter)
 --

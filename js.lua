3b44448d97bdf5fbf1f683ca846ceba4419777dc
demonityV2 <ytfazezachary7@gmail.com>
2025-03-07 18:07:50 -0500

    Update js.lua

--- js.lua
+++ js.lua
@@ -12,11 +12,11 @@ for _, v in pairs(getgc(true)) do
         end
     end
 end
-getgenv().LPHts = {jumpshoot = false}
+getgenv().Novas = {jumpshoot = false}
 local JS = debug.getupvalues(modules.Character.updateCharacter)
 --
 getfenv(1).old = hookfunction(JS[22].IsGrounded, function(...)
-if novas.jumpshoot then
+if Novas.jumpshoot then
     debug.setupvalue(getfenv(1).old, 1, true)
 end
     return getfenv(1).old(...)
